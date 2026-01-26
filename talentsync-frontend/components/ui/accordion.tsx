42a09ad0044114c1a51c70a54154802164cbc6f5
Chinmay Kudalkar <chinmaykudalkarrr@gmail.com>
2026-01-26 11:55:00 +0530

    Update accordion.tsx

--- talentsync-frontend/components/ui/accordion.tsx
+++ talentsync-frontend/components/ui/accordion.tsx
@@ -51,4 +51,3 @@ const AccordionContent = React.forwardRef<
 AccordionContent.displayName = AccordionPrimitive.Content.displayName;
 
 export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
-
