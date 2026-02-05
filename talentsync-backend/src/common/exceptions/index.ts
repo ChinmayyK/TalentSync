6ff340077d313b448b51f0a65e89b63a7c6ab429
Chinmay Kudalkar <chinmaykudalkarrr@gmail.com>
2026-02-05 12:33:00 +0530

    Update index.ts

--- talentsync-backend/src/common/exceptions/index.ts
+++ talentsync-backend/src/common/exceptions/index.ts
@@ -3,3 +3,4 @@ export { BusinessException } from './business.exception';
 export { ValidationException } from './validation.exception';
 export { NotFoundException } from './not-found.exception';
 export { UnauthorizedException } from './unauthorized.exception';
+
